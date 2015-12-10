65215514b8fe544562bdbb6937739c0629f110b6
Tóth Dávid <skot9212@gmail.com>
2015-12-10 20:58:37 +0100

    BNO

---
+++ core/src/main/java/hu/unideb/hospitalnet/core/dao/BnoDao.java
@@ -0,0 +1,9 @@
+package hu.unideb.hospitalnet.core.dao;
+
+import hu.unideb.hospitalnet.core.entity.Bno;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BnoDao extends JpaRepository<Bno, Long> {
+
+}
---
+++ core/src/main/java/hu/unideb/hospitalnet/core/entity/Bno.java
@@ -0,0 +1,36 @@
+package hu.unideb.hospitalnet.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bno")
+public class Bno extends BaseEntity{
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "kod10")
+	String kod10;
+	
+	@Column(name = "bno_name")
+	String name;
+
+	public String getKod10() {
+		return kod10;
+	}
+
+	public void setKod10(String kod10) {
+		this.kod10 = kod10;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
