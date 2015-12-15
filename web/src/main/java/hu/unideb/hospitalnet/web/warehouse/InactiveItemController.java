623970731420459f7d53db7fc71de4600ea7558b
Péter Dudás <dupetya@gmail.com>
2015-12-15 20:41:07 +0100

    Shipped items in statistic

--- web/src/main/java/hu/unideb/hospitalnet/web/warehouse/InactiveItemController.java
+++ web/src/main/java/hu/unideb/hospitalnet/web/warehouse/InactiveItemController.java
@@ -13,23 +13,29 @@ import org.primefaces.event.SelectEvent;
 import org.primefaces.model.LazyDataModel;
 
 import hu.unideb.hospitalnet.service.ItemManager;
+import hu.unideb.hospitalnet.service.WarehouseStatManager;
+import hu.unideb.hospitalnet.service.stats.vo.WarehouseStatVo;
+import hu.unideb.hospitalnet.service.util.DateUtil;
 import hu.unideb.hospitalnet.vo.ItemVo;
 
 @ViewScoped
 @ManagedBean(name = "inactiveItemView")
-public class InactiveItemController implements Serializable{
+public class InactiveItemController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private ItemVo selectedItem;
-	
+
 	private List<ItemVo> selectedItems;
-	
+
 	@ManagedProperty("#{itemManager}")
 	private ItemManager itemManager;
-	
+
 	@ManagedProperty("#{lazyItemModel}")
 	private LazyDataModel<ItemVo> lazyItemModel;
+
+	@ManagedProperty("#{warehouseStatManager}")
+	private WarehouseStatManager warehouseStatManager;
 
 	public LazyDataModel<ItemVo> getLazyItemModel() {
 		return lazyItemModel;
@@ -50,7 +56,7 @@ public class InactiveItemController implements Serializable{
 	public void setItemManager(ItemManager itemManager) {
 		this.itemManager = itemManager;
 	}
-	
+
 	public void onRowSelect(SelectEvent e) {
 		selectedItem = (ItemVo) e.getObject();
 	}
@@ -70,50 +76,67 @@ public class InactiveItemController implements Serializable{
 	public void setSelectedItems(List<ItemVo> selectedItems) {
 		this.selectedItems = selectedItems;
 	}
-	
-	public void setItemsToInactive(){
+
+	public WarehouseStatManager getWarehouseStatManager() {
+		return warehouseStatManager;
+	}
+
+	public void setWarehouseStatManager(WarehouseStatManager warehouseStatManager) {
+		this.warehouseStatManager = warehouseStatManager;
+	}
+
+	public void setItemsToInactive() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		try {
 			itemManager.setItemsStatus(selectedItems, "elszállítva");
-			context.addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO, "Sikeres elszállítás!", getSuccesMassage()));
+			saveStat(selectedItems.stream().mapToInt(itm -> itm.getNumberOfUnitNow()).sum());
+			context.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Sikeres elszállítás!", getSuccesMassage()));
 		} catch (Exception e) {
 			e.getMessage();
 			e.printStackTrace();
-			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Sikeres elszállítás!", getErrorMassage()));
+			context.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Sikeres elszállítás!", getErrorMassage()));
 		}
 	}
-	
-	public String getSuccesMassage(){
+
+	private void saveStat(int shippedAway) {
+		WarehouseStatVo whStatVo = new WarehouseStatVo();
+		whStatVo.setDayOfOccurence(DateUtil.getStartOfDay(DateUtil.now()));
+		whStatVo.setOrdered(0L);
+		whStatVo.setShippedAway(Long.valueOf(shippedAway));
+		whStatVo.setGivenToPatients(0L);
+		warehouseStatManager.addStat(whStatVo);
+	}
+
+	public String getSuccesMassage() {
 		StringBuilder sb = new StringBuilder();
-		
-		if(selectedItems.size() > 1){
+
+		if (selectedItems.size() > 1) {
 			sb.append("Elszállított tételek azonosítója: ");
-		}
-		else{
+		} else {
 			sb.append("Elszállított tétel azonosítója: ");
 		}
-		for(ItemVo ivo : selectedItems){
+		for (ItemVo ivo : selectedItems) {
 			sb.append(ivo.getId());
 		}
-			
+
 		return sb.toString();
 	}
-	
-	public String getErrorMassage(){
+
+	public String getErrorMassage() {
 		StringBuilder sb = new StringBuilder();
-		
-		if(selectedItems.size() > 1){
+
+		if (selectedItems.size() > 1) {
 			sb.append("Nem sikerült leselejtezni a következő azonosítójú tételeket: ");
-		}
-		else{
+		} else {
 			sb.append("Nem sikerült leselejtezni a következő azonosítójú tételt: ");
 		}
-		for(ItemVo ivo : selectedItems){
+		for (ItemVo ivo : selectedItems) {
 			sb.append(ivo.getId());
 		}
-			
+
 		return sb.toString();
 	}
-	
-}
 
+}
